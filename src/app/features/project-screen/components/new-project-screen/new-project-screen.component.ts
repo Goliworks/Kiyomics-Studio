e4f23138666e5ebfd19c895c2eecc51c9f00822e
Guillaume (Goliver) Golay <goliworks@gmail.com>
2023-02-25 01:02:54 +0100

    refactor: use radio buttons instead of dropdown

--- src/app/features/project-screen/components/new-project-screen/new-project-screen.component.ts
+++ src/app/features/project-screen/components/new-project-screen/new-project-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './new-project-screen.component.html',
   styleUrls: ['./new-project-screen.component.scss'],
 })
-export class NewProjectScreenComponent {
+export class NewProjectScreenComponent implements OnInit {
   projectForm = new FormGroup({
     name: new FormControl(''),
     location: new FormControl(''),
@@ -16,9 +16,9 @@ export class NewProjectScreenComponent {
   });
 
   projectFormat: ProjectFormat[] = [
-    { name: 'HD 1280 x 720', width: 1280, height: 720 },
-    { name: 'Full HD 1920 x 1080', width: 1920, height: 1080 },
-    { name: 'Custom', width: 0, height: 0 },
+    { name: 'HD', key: 'hd', width: 1280, height: 720 },
+    { name: 'Full HD', key: 'full-hd', width: 1920, height: 1080 },
+    { name: 'Custom', key: 'custom', width: 0, height: 0 },
   ];
 
   changeFormat(e: ChangeFormatEvent) {
@@ -29,15 +29,21 @@ export class NewProjectScreenComponent {
   changeSize() {
     this.projectForm.get('format')?.patchValue(this.projectFormat[2]); // Change format field to "Custom".
   }
+
+  ngOnInit() {
+    this.projectForm.get('format')?.patchValue(this.projectFormat[0]);
+    this.changeFormat({ originalEvent: null, value: this.projectFormat[0] });
+  }
 }
 
 interface ProjectFormat {
   name: string;
+  key: string;
   width: number;
   height: number;
 }
 
 interface ChangeFormatEvent {
-  originalEvent: Event;
+  originalEvent: Event | null;
   value: ProjectFormat;
 }
--- src/app/features/project-screen/project-screen.module.ts
+++ src/app/features/project-screen/project-screen.module.ts
@@ -6,10 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'primeng/carousel';
 import { NewProjectScreenComponent } from './components/new-project-screen/new-project-screen.component';
 import { InputTextModule } from 'primeng/inputtext';
-import { DropdownModule } from 'primeng/dropdown';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @NgModule({
   declarations: [ProjectScreenComponent, NewProjectScreenComponent],
@@ -19,10 +19,10 @@ import { InputNumberModule } from 'primeng/inputnumber';
     CarouselModule,
     DialogModule,
     InputTextModule,
-    DropdownModule,
     ReactiveFormsModule,
     ButtonModule,
     InputNumberModule,
+    RadioButtonModule,
   ],
   exports: [ProjectScreenComponent],
 })
